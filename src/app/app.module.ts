dcf4f7bcdc4ae559805d5f830eaa0c9feb08e530
Stephanie Hope <steph.hope@gmail.com>
2019-07-31 22:20:12 +1200

    Initial commit. Created template app and a few pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ProjectsComponent } from './projects/projects.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HttpClientModule} from "@angular/common/http";
+import { NewProjectComponent } from './new-project/new-project.component';
+
+const appRoutes : Routes = [
+  //this is the route to each of the components
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'list-project',
+    component: NewProjectComponent
+  },
+  //this one is the default
+  {
+    path: '',
+    component: ProjectsComponent,
+    pathMatch: 'full'
+  },
+  //and this is for page not found
+  {
+    path: '**',
+    component: NotFoundComponent,
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ProjectsComponent,
+    NotFoundComponent,
+    NewProjectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbCollapseModule,
+    //this one is so that the routes are actually connected
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Project} from "./model/project";
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css']
+})
+export class ProjectsComponent implements OnInit {
+
+projects: Project[] = [];
+
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.getAllProjects();
+  }
+
+  public getAllProjects(){
+    let url = "http://localhost:8082/listings/all";
+    this.http.get<Project[]>(url).subscribe(
+      res => {
+        this.projects = res;
+      },
+      err => {
+        alert("an error occured while getting project listings")
+      }
+    );
+
+  }
+
+}
